c459109c4a443bea9b6f7e2fc96faa50e347f458
Oleksandr-Kotenko <Oleksandr_Kotenko@epam.com>
2023-10-10 01:02:29 +0300

    add products routes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
