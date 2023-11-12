95c0df6473800d7f74ea92eee880298bb7917719
Oleksandr-Kotenko <Oleksandr_Kotenko@epam.com>
2023-11-13 01:11:10 +0200

    add axios interceptor. Add Authorize header

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -33,6 +33,9 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
       params: {
         name: encodeURIComponent(file?.name || ""),
       },
+      headers: {
+        Authorization: `Basic ${localStorage.getItem("token") || null}`,
+      },
     });
     console.log("File to upload: ", file?.name);
     console.log("Uploading to: ", response.data);
