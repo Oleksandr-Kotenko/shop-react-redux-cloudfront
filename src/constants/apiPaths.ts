05f304d5fe677a18dbb1ccad764c48f0c41d64cc
Oleksandr-Kotenko <Oleksandr_Kotenko@epam.com>
2023-12-11 01:20:10 +0200

    integration with cart service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://xboqc5j7cd.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://10vvjlt2c6.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://j32kxuybgg.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
